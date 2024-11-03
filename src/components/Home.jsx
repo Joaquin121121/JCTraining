8270f2879e2f7c8f801cbbfe84233ca3ef0233fb
Joaquin del Rio <joaquindelrio121@gmail.com>
2024-11-03 10:40:08 -0300

    3-11

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,12 +12,109 @@ function Home() {
       style={{
         height: "100vh",
         width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        padding: "60px 7.5%",
+        backgroundColor: "#F5F5F5",
       }}
     >
-      <button onClick={logout}>Logout</button>
+      <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+        <h1>Hola Julio</h1>
+        <img
+          style={{ height: "auto", width: "30px", objectFit: "cover" }}
+          src="/images/hand.png"
+          alt="waving-hand"
+        />
+      </div>
+      <h2 style={{ marginBottom: "20px" }}>Mi Semana</h2>
+      <div className="calendar">
+        <div className="calendarRow">
+          <div className="card dayCard">
+            <p className="day">Lunes</p>
+            <p className="activity">Rodar Suave</p>
+            <img
+              src="/images/check.png"
+              alt=""
+              style={{
+                marginTop: "20px",
+                height: "36px",
+                width: "auto",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+          <div className="card dayCard">
+            <p className="day">Martes</p>
+            <p className="restDay">Descanso</p>
+            <img
+              src="/images/check.png"
+              alt=""
+              style={{
+                marginTop: "20px",
+                height: "36px",
+                width: "auto",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+          <div className="card dayCard">
+            <p className="day">Miércoles</p>
+            <p className="activity">5x5'</p>
+            <img
+              src="/images/check.png"
+              alt=""
+              style={{
+                marginTop: "20px",
+                height: "36px",
+                width: "auto",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        </div>
+        <div className="calendarRow">
+          <div className="card dayCard">
+            <p className="day">Lunes</p>
+            <p className="activity">Rodar Suave</p>
+            <img
+              src="/images/check.png"
+              alt=""
+              style={{
+                marginTop: "20px",
+                height: "36px",
+                width: "auto",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+          <div className="card dayCard">
+            <p className="day">Lunes</p>
+            <p className="activity">Rodar Suave</p>
+            <img
+              src="/images/check.png"
+              alt=""
+              style={{
+                marginTop: "20px",
+                height: "36px",
+                width: "auto",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+          <div className="card dayCard">
+            <p className="day">Lunes</p>
+            <p className="activity">Rodar Suave</p>
+            <img
+              src="/images/check.png"
+              alt=""
+              style={{
+                marginTop: "20px",
+                height: "36px",
+                width: "auto",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
